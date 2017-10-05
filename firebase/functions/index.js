db0ede3f82d1ffd29d9c7d77320e236746be6b9f
Daniel Antonio Conte <danconte72>
2017-10-05 10:48:42 -0300

    FIX: Closes #30, if user is deleted its account unexpectedly remains on authentication db Changes: - Added a Cloud Function who catches User deleted in the database and trigger the auth delete;

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const gmailEmail = encodeURIComponent(functions.config().gmail.email);
@@ -29,3 +30,20 @@ function sendWelcomeEmail(email, displayName,user)   {
   });
 }
 
+exports.removeUser = functions.database.ref('/user/{pushId}/email').onDelete(event => {
+  const email = event.data.previous.val();
+  admin.auth().getUserByEmail(email)
+  .then(function(user) {
+    const uid = user.uid;
+    admin.auth().deleteUser(uid)
+    .then(function() {
+      console.log("User Deleted");
+    })
+    .catch(function(error) {
+      console.log("Error Delete:", error);
+    });
+  })
+  .catch(function(error) {
+    console.log("Error List:", error);  
+  });
+});
